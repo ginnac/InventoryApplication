00cd5771ddeca98f5396fc3fa3e155e3c251ee1e
ginnac <ginnacampbell1990@gmail.com>
2022-12-16 18:33:45 -0700

    Parts Modify method (inhouse and outsourced) saving data into allparts array.

--- src/main/java/project/inventoryapp/controllers/InHouseController.java
+++ src/main/java/project/inventoryapp/controllers/InHouseController.java
@@ -64,9 +64,30 @@ public class InHouseController implements Initializable {
 
     public void inHouseOnClickSaveBtn(ActionEvent actionEvent) {
 
-        InHouse inHouseObj = new InHouse(index, nameBox.getText(), Double.parseDouble(priceBox.getText()), Integer.parseInt(invBox.getText()), Integer.parseInt(minBox.getText()),Integer.parseInt(maxBox.getText()), Integer.parseInt(machineIdBox.getText()));
-        Inventory.addPart(inHouseObj);
-        pageLoader(actionEvent, "/project/inventoryapp/inventory.fxml", "button");
+        if(InventoryController.getPageTitle() == "Modify Part"){
+            //get id from screen
+            int idToSave = Integer.parseInt(idBox.getText());
+            //find object with that id in parts list. First loop
+            for(int i = 0; i<Inventory.getAllParts().size(); i++){
+                //then compare id in UI vs Part's id in Parts list. If they match update all values with the exception of Id.
+                if(idToSave == Inventory.getAllParts().get(i).getId()){
+                    Inventory.getAllParts().get(i).setName(nameBox.getText());
+                    Inventory.getAllParts().get(i).setStock(Integer.parseInt(invBox.getText()));
+                    Inventory.getAllParts().get(i).setPrice(Double.parseDouble(priceBox.getText()));
+                    Inventory.getAllParts().get(i).setMin(Integer.parseInt(minBox.getText()));
+                    Inventory.getAllParts().get(i).setMax(Integer.parseInt(maxBox.getText()));
+                    ((InHouse)Inventory.getAllParts().get(i)).setMachineId(Integer.parseInt(machineIdBox.getText()));
+                }
+            }
+            // redirect to inventory screen
+            pageLoader(actionEvent, "/project/inventoryapp/inventory.fxml", "button");
+
+        }
+        else {
+            InHouse inHouseObj = new InHouse(index, nameBox.getText(), Double.parseDouble(priceBox.getText()), Integer.parseInt(invBox.getText()), Integer.parseInt(minBox.getText()), Integer.parseInt(maxBox.getText()), Integer.parseInt(machineIdBox.getText()));
+            Inventory.addPart(inHouseObj);
+            pageLoader(actionEvent, "/project/inventoryapp/inventory.fxml", "button");
+        }
     }
 
     public void InHouseOnClickCancelBtn(ActionEvent actionEvent) {
